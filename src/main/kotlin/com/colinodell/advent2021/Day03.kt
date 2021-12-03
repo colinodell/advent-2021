04e40e5eccda54ebd87abdba4a002bcc198ef7f7
Colin O'Dell <colinodell@gmail.com>
2021-12-03 11:25:50 -0500

    Refine implementation; add comments

--- src/main/kotlin/com/colinodell/advent2021/Day03.kt
+++ src/main/kotlin/com/colinodell/advent2021/Day03.kt
@@ -6,27 +6,35 @@ class Day03 (private val input: List<String>) {
     fun solvePart1(): Int {
         var gammaRate = ""
 
+        // Construct a new binary string with the most common bit at each position
         for (bitPosition in 0 until bitSize) {
             gammaRate += input.mostCommonBit(bitPosition)
         }
 
+        // Convert binary string to decimal
         val gammaRateDecimal = Integer.parseInt(gammaRate, 2)
+        // Clever way to find the epsilon rate, since the sum of both rates will always equal
+        // one less than the maximum value that can be stored in `bitSize` bits
         val epsilonRateDecimal = (2 shl bitSize - 1) - gammaRateDecimal - 1
 
         return gammaRateDecimal * epsilonRateDecimal
     }
 
     fun solvePart2(): Int {
+        // Set to 1, since we'll be multiplying the answer as we go
         var answer = 1
 
-        for (getMostCommon in listOf(true, false)) {
+        // First we operate with the most common bit, then we operate with the least common bit
+        for (mostCommon in listOf(true, false)) {
+            // Clone the list of numbers
             var numbers = input.toMutableList()
 
+            // At each bit position...
             for (bitPosition in 0 until bitSize) {
-                val desiredBit = if (getMostCommon) numbers.mostCommonBit(bitPosition) else numbers.leastCommonBit(bitPosition)
                 // Remove all numbers that don't have the desired bit
-                numbers = numbers.filter { it.get(bitPosition) == desiredBit }.toMutableList()
+                numbers = numbers.filter { (it.get(bitPosition) == numbers.mostCommonBit(bitPosition)) == mostCommon }.toMutableList()
 
+                // Only one number left? It must be our answer
                 if (numbers.size == 1) {
                     answer *= numbers[0].toInt(2)
                     break
@@ -42,12 +50,5 @@ class Day03 (private val input: List<String>) {
         val zeroesCount = this.size - onesCount
 
         return if (onesCount >= zeroesCount) '1' else '0'
-    }
-
-    private fun List<String>.leastCommonBit(bitPosition: Int): Char {
-        val onesCount = this.count { it[bitPosition] == '1' }
-        val zeroesCount = this.size - onesCount
-
-        return if (onesCount < zeroesCount) '1' else '0'
     }
 }
